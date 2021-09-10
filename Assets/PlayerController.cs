da86f895c947cf15c1e6cf62d3713fbcbe8ae3eb
KaisarFS <kaisar.10rpl2.2019@gmail.com>
2021-09-10 20:21:44 +0700

    initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 10f;
+
+    private float xInput;
+    private float yInput;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ProcessInputs()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
+
+    private void Move()
+    {
+        rb.AddForce(new Vector3(xInput, 0f, y   Input) * moveSpeed);
+    }
+}
